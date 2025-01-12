31426d975b9b1df7c38a9e02fded2c22ff970381
koogit <kht999123@gmail.com>
2025-01-12 15:39:17 +0900

    2025.01.11 토 미팅

--- app.js
+++ app.js
@@ -11,167 +11,163 @@
  * npm install http
  * npm install cors
  * npm install http express cors mysql : 한방에 설치 가능
- * 
+ *
  * node 실행 : node app.js
  * node 실행 중단 : Ctrl+c
  */
-const http = require('http'); // https로 변경할 수 있습니다.
-const express = require('express');
-const cors = require('cors');
+const http = require("http"); // https로 변경할 수 있습니다.
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-// 모든 도메인에서 제한 없이 해당 서버에 요청을 보내고 응답하도록 허용 
+// 모든 도메인에서 제한 없이 해당 서버에 요청을 보내고 응답하도록 허용
 // 특정 도메인에만 허용하려면 corsOptions 를 이용하여 설정 가능
 app.use(cors());
 
-// JSON 형식의 본문 파싱 미들웨어 추가 
+// JSON 형식의 본문 파싱 미들웨어 추가
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // mariaDB 연결
-const maria = require('./database/connect/mariadb');
+const maria = require("./database/connect/mariadb");
 maria.connect();
 
 const port = 3000;
 const server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
 
 // 모든 사용자 정보 (GET)
-app.get('/api/users', async (req, res) => {
-  maria.query('select * from t_user', function(err, rows, fields) {
-    if(!err){
+app.get("/api/users", async (req, res) => {
+  maria.query("select * from t_user", function (err, rows, fields) {
+    if (!err) {
       // console.log("succ", rows);
       // array 로 보내줌
       res.send(rows);
-    }
-    else {
+    } else {
       console.log("err : ", err);
     }
   });
-})
+});
 
 // 특정 사용자 정보 (GET)
-app.get('/api/users/:username', async (req, res) => {
+app.get("/api/users/:username", async (req, res) => {
   // console.log('요청받은 파라미터 :>> ', req.params);
   // let { username, password } = req.params; // 요청 파라미터 중에서 username, password 변수 선언
   // const sql = `select * from t_user where username = '${username}' and password = '${password}'`;
 
-  let { username } = req.params;  // 요청 파라미터 중에서 username 변수 선언
-  
+  let { username } = req.params; // 요청 파라미터 중에서 username 변수 선언
+
   const sql = `select * from t_user where username = '${username}'`; // 백틱(`)을 사용하면 문자열 안에 변수를 넣을 수 있음
-  maria.query(sql, function(err, rows, fields) {
-    if(!err){
+  maria.query(sql, function (err, rows, fields) {
+    if (!err) {
       // array 타입이 아닌 object 타입으로 데이터 발송
       // console.log("succ", rows);
       res.send(rows[0]);
-    }
-    else {
+    } else {
       console.log("err : ", err);
     }
   });
-})
+});
 
 // 사용자 등록 (POST)
-app.post('/api/users/add', async (req, res) => {
-  console.log('/api/users/add :>> ', req.body);
+app.post("/api/users/add", async (req, res) => {
+  console.log("/api/users/add :>> ", req.body);
   // 요청받은 body 파라미터 중에서 username, email, password 변수 선언
-  const {username, email, password} = req.body
+  const { username, email, password } = req.body;
 
-  const sql = 'INSERT INTO t_user (username, email, profile, created_at, password) VALUES(?, ?, NULL, now(), ?)';
+  const sql =
+    "INSERT INTO t_user (username, email, profile, created_at, password) VALUES(?, ?, NULL, now(), ?)";
   // 위 쿼리 ? 자리에 들어갈 파라미터를 순서!!!대로 params array 에 넣어줌
   var params = [username, email, password];
 
-  maria.query(sql, params, function(err, rows, fields) {
-    if(!err){
+  maria.query(sql, params, function (err, rows, fields) {
+    if (!err) {
       console.log("succ", rows);
-      res.send({ok: true, username: username});
-    }
-    else {
+      res.send({ ok: true, username: username });
+    } else {
       console.log("err : ", err);
     }
   });
-})
+});
 
 // 사용자 수정
 // 사용자 삭제
 
 // 모든 게시글 list 조회
-app.get('/api/articles', async (req, res) => {
-  maria.query('select * from t_board', function(err, rows, fields) {
-    if(!err){
+app.get("/api/articles", async (req, res) => {
+  maria.query("select * from t_board", function (err, rows, fields) {
+    if (!err) {
       // console.log("succ", rows);
       res.send(rows);
-    }
-    else {
+    } else {
       console.log("err : ", err);
     }
   });
-})
+});
 
 // 특정 게시글 조회 (GET)
-app.get('/api/articles/:id', async (req, res) => {
+app.get("/api/articles/:id", async (req, res) => {
   const { id } = req.params;
   const sql = `select * from t_board where id = '${id}'`; // 백틱(`)을 사용하면 문자열 안에 변수를 넣을 수 있음
-  maria.query(sql, function(err, rows, fields) {
-    if(!err){
+  maria.query(sql, function (err, rows, fields) {
+    if (!err) {
       // console.log("succ", rows);
       res.send(rows[0]);
-    }
-    else {
+    } else {
       console.log("err : ", err);
     }
   });
-})
+});
 
 // 게시글 등록(POST)
-app.post('/api/articles/add', async (req, res) => {
+app.post("/api/articles/add", async (req, res) => {
   // console.log('/api/articles/add :>> ', req.body);
-  const {type, title, contents, username} = req.body
-  const sql = 'INSERT INTO t_board (type, title, contents, writer, created_at, updated_at) VALUES(?, ?, ?, ?, now(), now())';
+  const { type, title, contents, username } = req.body;
+  const sql =
+    "INSERT INTO t_board (type, title, contents, writer, created_at, updated_at) VALUES(?, ?, ?, ?, now(), now())";
   const params = [type, title, contents, username];
 
-  maria.query(sql, params, function(err, rows, fields) {
-    if(!err){
+  maria.query(sql, params, function (err, rows, fields) {
+    if (!err) {
       console.log("succ", rows);
-      res.send({ok: true});
-    }
-    else {
+      res.send({ ok: true });
+    } else {
       console.log("err : ", err);
     }
   });
-})
+});
 
 // 게시글 수정
 // 게시글 삭제
 
 // 샘플 api
-app.get('/api/idols/:group', async (req, res) => {
+app.get("/api/idols/:group", async (req, res) => {
   // 파라미터 정보 가져오기
   let { group } = req.params;
 
-  if (group === 'monsta-x') {
+  if (group === "monsta-x") {
     let data = [
-      {name: "셔누", age: 31},
-      {name: "민혁", age: 30},
-      {name: "기현", age: 30},
-      {name: "형원", age: 29},
-      {name: "주헌", age: 29},
-      {name: "아이엠", age: 27},
+      { name: "셔누", age: 31 },
+      { name: "민혁", age: 30 },
+      { name: "기현", age: 30 },
+      { name: "형원", age: 29 },
+      { name: "주헌", age: 29 },
+      { name: "아이엠", age: 27 },
     ];
     // 데이터 보내기
     res.send(data);
-  } else if (group === 'red-velvet') {
+  } else if (group === "red-velvet") {
     let data = [
-      {name: "웬디", age: 29},
-      {name: "아이린", age: 32},
-      {name: "슬기", age: 29},
-      {name: "조이", age: 27},
-      {name: "예리", age: 24},
+      { name: "웬디", age: 29 },
+      { name: "아이린", age: 32 },
+      { name: "슬기", age: 29 },
+      { name: "조이", age: 27 },
+      { name: "예리", age: 24 },
     ];
     // 데이터 보내기
     res.send(data);
   } else {
-    res.send('존재하지 않는 그룹입니다.')
+    res.send("존재하지 않는 그룹입니다.");
   }
-})
+});
